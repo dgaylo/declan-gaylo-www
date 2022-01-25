ac81ccd5da02346a732b664087a5e838ea630374
dgaylo <dgaylo@mit.edu>
2022-01-25 01:21:53 -0500

    First commit

---
+++ pelicanconf.py
@@ -0,0 +1,41 @@
+AUTHOR = 'Declan Gaylo'
+SITENAME = 'Declan Gaylo'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'America/New_York'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Plugins
+PLUGIN_PATHS = ['./pelican-plugins']
+PLUGINS = ['i18n_subsites']
+
+# Theme setup
+THEME = './pelican-themes/pelican-bootstrap3'
+BOOTSTRAP_THEME = 'lumen'
+JINJA_ENVIRONMENT = {'extensions': ['jinja2.ext.i18n']}
+
+
+# Blogroll
+LINKS = (('Pelican', 'https://getpelican.com/'),
+         ('Python.org', 'https://www.python.org/'),
+         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
