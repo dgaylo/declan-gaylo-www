a43f7d0b911b26c31c0313259530bd68206e3991
dgaylo <dgaylo@mit.edu>
2022-01-25 22:52:45 -0500

    added publications page

---
+++ content/pages/publications.md
@@ -0,0 +1,20 @@
+Title: Publications
+slug: publications
+modified: 2022-01-25 22:00
+
+
+### Jounal Papers
+
+Gaylo, D. B., Hendrickson, K., & Yue, D. K.P. (2021). Effects of power-law entrainment on bubble fragmentation cascades. *Journal of Fluid Mechanics, 917*, R1. doi:[10.1017/jfm.2021.333](https://doi.org/10.1017/jfm.2021.333)
+
+### Conference Presentations
+
+Gaylo, D. B., Hendrickson, K., & Yue, D. K.P. (2021, November). *Effects of split power-law entrainment on bubble fragmentation cascades*. Presented at the 74th Annual Meeting of the APS Division of Fluid Dynamics, Phoenix, AZ.
+
+Gaylo, D. B., Hendrickson, K., & Yue, D. K.P. (2020, November). *Eulerian Label Advection method for bubble tracking in two-fluid Navier-Stokes simulations*. Presented at the 73rd Annual Meeting of the APS Division of Fluid Dynamics, Chicago, IL.
+
+### Academic Theses
+
+Gaylo, D. B. (2021). *Effects of power-law entrainment on bubble fragmentation cascades* (Master's thesis). Massachusetts Institute of Technology, Cambridge. doi:[1721.1/139428](https://hdl.handle.net/1721.1/139438)
+
+Gaylo, D. B., & Roske, D. (2019). *Pressure Effects of Transom Lift Devices on Prismatic Planing Hulls* (Bachelor's thesis). Webb Institute, Glen Cove, NY. [Download]({static}/downloads/publications/Pressure Effects of Transom Lift Devices on Prismatic Planing Hulls.pdf)
--- pelicanconf.py
+++ pelicanconf.py
@@ -15,6 +15,12 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
+# Setup URLS
+ARTICLE_URL = '{category}/{date:%Y}/{date:%m}/{slug}/index.html'
+ARTICLE_SAVE_AS = '{category}/{date:%Y}/{date:%m}/{slug}/index.html'
+PAGE_URL = '{slug}/'
+PAGE_SAVE_AS = '{slug}.html'
+
 # Plugins
 PLUGIN_PATHS = ['./pelican-plugins']
 PLUGINS = ['i18n_subsites']
@@ -24,16 +30,22 @@ THEME = './pelican-themes/pelican-bootstrap3'
 BOOTSTRAP_THEME = 'lumen'
 JINJA_ENVIRONMENT = {'extensions': ['jinja2.ext.i18n']}
 
+DISPLAY_PAGES_ON_MENU = False
+MENUITEMS = (
+    ('Publications', '/publications.html'),
+)
 
 # Blogroll
-LINKS = (('Pelican', 'https://getpelican.com/'),
-         ('Python.org', 'https://www.python.org/'),
-         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
-         ('You can modify those links in your config file', '#'),)
+#LINKS = (('Pelican', 'https://getpelican.com/'),
+#         ('Python.org', 'https://www.python.org/'),
+#         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
+#         ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+#SOCIAL = (
+#    ('Email', 'mailto:dgaylo@mit.edu', 'dgaylo@mit.edu'),
+#    ("Github", "https://github.com/dgaylo/", "Elegant Github Repository"),
+#)
 
 DEFAULT_PAGINATION = False
 
