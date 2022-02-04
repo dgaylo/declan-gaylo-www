ab86c9d02fa08e85db113316f6e81831de12fa94
dgaylo <dgaylo@mit.edu>
2022-02-03 21:19:39 -0500

    Added source code link

--- pelicanconf.py
+++ pelicanconf.py
@@ -43,6 +43,8 @@ JINJA_ENVIRONMENT = {'extensions': ['jinja2.ext.i18n']}
 FONT_AWESOME_KIT = 'b3b7784092'
 DISPLAY_ARTICLE_LIST_ON_INDEX = False
 
+SOURCE_CODE_URL= 'https://github.com/dgaylo/declan-gaylo-www'
+
 CUSTOM_CSS = 'static/custom.css'
 
 THEME_TEMPLATES_OVERRIDES = [ './content/theme/' ]
