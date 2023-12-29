522e07206c3b569dcd8ba828ef863be9ea50dad4
Declan Gaylo <54426925+dgaylo@users.noreply.github.com>
2023-12-28 22:16:12 -0500

    Dev Server (#8)

--- publishconf_dev.py
+++ publishconf_dev.py
@@ -6,7 +6,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'https://dgaylo.scripts.mit.edu/home'
+SITEURL = 'https://dgaylo.scripts.mit.edu/dev/home'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
@@ -15,7 +15,7 @@ CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
 DELETE_OUTPUT_DIRECTORY = True
 
 # Google Analytics
-GOGGLE_DEBUG_MODE = False
+GOGGLE_DEBUG_MODE = True
 
 # source code link (with hash)
 sha = git.Repo(search_parent_directories=True).head.object.hexsha
