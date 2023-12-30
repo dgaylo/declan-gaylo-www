b03e8f48b05b8a9be03f4e578ea9f0e840743921
Declan Gaylo <dgaylo@mit.edu>
2023-12-29 21:59:29 -0500

    Remove htaccess

--- pelicanconf.py
+++ pelicanconf.py
@@ -30,7 +30,7 @@ AUTHOR_SAVE_AS = ''
 TAG_SAVE_AS = ''
 ARCHIVES_SAVE_AS = ''
 
-STATIC_PATHS = ['images','extra','static','files']
+STATIC_PATHS = ['images','static','files']
 ARTICLE_PATHS  = ['posts']
 PAGE_PATHS = ['pages']
 
@@ -134,9 +134,6 @@ DEFAULT_PAGINATION = False
 
 # Cache settings using .htaccess files
 EXTRA_PATH_METADATA = {
-    'extra/htaccess/theme_css': {'path': 'theme/css/.htaccess'},
-    'extra/htaccess/theme_js': {'path': 'theme/js/.htaccess'},
-    'extra/htaccess/images': {'path': 'images/.htaccess'},
     'static/robots.txt': {'path': 'robots.txt'}
 }
 
