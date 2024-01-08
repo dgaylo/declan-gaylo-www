3d28be177b433900ea4fc06f26b819f1d8107376
Declan Gaylo <54426925+dgaylo@users.noreply.github.com>
2024-01-07 22:48:53 -0500

    Turn off pelican generated index (#19)

--- pelicanconf.py
+++ pelicanconf.py
@@ -29,6 +29,7 @@ PAGE_SAVE_AS = '{slug}/index.html'
 AUTHOR_SAVE_AS = ''
 TAG_SAVE_AS = ''
 ARCHIVES_SAVE_AS = ''
+INDEX_SAVE_AS = ''
 
 STATIC_PATHS = ['images','static','files']
 ARTICLE_PATHS  = ['posts']
