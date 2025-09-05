b012bfe8b42f4fe78e33306305b9a284b0197cc0
Declan Gaylo <dgaylo@mit.edu>
2025-09-04 21:26:50 -0400

    Remove spaces from Resume link

--- pelicanconf.py
+++ pelicanconf.py
@@ -117,9 +117,10 @@ LINKS = (
 )
 
 # Social widget
+RESUME = '/files/Declan Gaylo%20-%20Resume.pdf'
 SOCIAL_WIDGET_NAME = 'Profiles'
 SOCIAL = (
-    ('Resume', '/files/Declan Gaylo - Resume.pdf', 'bi bi-file-earmark-pdf-fill'),
+    ('Resume', RESUME, 'bi bi-file-earmark-pdf-fill'),
     ('LinkedIn', 'https://www.linkedin.com/in/declan-gaylo', 'bi bi-linkedin'),
     ('Google Scholar', 'https://scholar.google.com/citations?hl=en&user=kA9LJygAAAAJ', 'bi bi-google'),
     ('GitHub', 'https://github.com/dgaylo/', 'bi bi-github'),
